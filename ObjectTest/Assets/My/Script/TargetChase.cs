693028bc8b9123e1b65450f8d2b558b3937a42fb
konata016 <k>
2019-05-10 17:27:03 +0900

    apowermanager

---
+++ ObjectTest/Assets/My/Script/TargetChase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetChase : MonoBehaviour
+{
+    public GameObject Target;
+    public Vector3 Pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 V3 = transform.position;
+        V3.x = Target.transform.position.x + Pos.x;
+        V3.y = Target.transform.position.y + Pos.y;
+        V3.z = Target.transform.position.z + Pos.z;
+        transform.position = V3;
+    }
+}
---
+++ ObjectTest/Assets/My/Script/anime2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anime2 : MonoBehaviour
+{
+    Vector3 Save;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Save = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Save = transform.position;
+
+        float x = transform.position.x - Save.x;
+        float y = transform.position.y - Save.y;
+
+        Save = transform.position;
+    }
+}
